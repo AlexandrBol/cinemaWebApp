0bdccce5f9b65d448eab2016d995f053f21e17a4
alexandrbolshakov <bolshakovalex130@gmail.com>
2017-05-25 17:17:02 +0300

    fix dependencies

--- src/main/Java/ua/org/bolshakov/controllers/LoginServlet.java
+++ src/main/Java/ua/org/bolshakov/controllers/LoginServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @WebServlet(name = "LoginServlet", urlPatterns={"/login"})
 public class LoginServlet extends HttpServlet {
--- src/main/webapp/pages/common/movies.jsp
+++ src/main/webapp/pages/common/movies.jsp
@@ -3,10 +3,9 @@
 <html>
 <head>
     <title>Title</title>
-    <jsp:include page="../tmpl/headtag.jspx" />
+
 </head>
 <body>
-<jsp:include page="../tmpl/head.jspx" />
 <center>
 <c:forEach items="${movieDTOList}" var="movies">
 <a href="${pageContext.servletContext.contextPath}/movie?id=${movies.id}">${movies.title}</a><br/>
