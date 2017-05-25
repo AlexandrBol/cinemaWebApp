dae45addbb320f4c4dfb049a9a87a9a85878d7ed
alexandrbolshakov <bolshakovalex130@gmail.com>
2017-05-25 17:40:19 +0300

    admin

--- src/main/Java/ua/org/bolshakov/controllers/LoginServlet.java
+++ src/main/Java/ua/org/bolshakov/controllers/LoginServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory; 
+import org.slf4j.LoggerFactory;
 
 @WebServlet(name = "LoginServlet", urlPatterns={"/login"})
 public class LoginServlet extends HttpServlet {
@@ -21,7 +21,7 @@ public class LoginServlet extends HttpServlet {
         request.getRequestDispatcher("pages/common/login.jsp").forward(request, response);
         if(userDTO != null && userDTO.getPassword().equals(password)){
             request.getSession().setAttribute("user", userDTO);
-            response.sendRedirect(request.getSession().getAttribute("url").toString());
+            response.sendRedirect(request.getSession().getAttribute("/pages/admin/admin.jsp").toString());
 
         }else{
             request.getSession().setAttribute("message", "Wrong users name or password");
