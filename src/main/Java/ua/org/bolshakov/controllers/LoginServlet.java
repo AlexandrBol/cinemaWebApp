5fa20a7c5381b3fcda6e23463966d50dc3cea3b9
alexandrbolshakov <bolshakovalex130@gmail.com>
2017-05-25 17:17:15 +0300

    fix dependencies

--- src/main/Java/ua/org/bolshakov/controllers/LoginServlet.java
+++ src/main/Java/ua/org/bolshakov/controllers/LoginServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerFactory; 
 
 @WebServlet(name = "LoginServlet", urlPatterns={"/login"})
 public class LoginServlet extends HttpServlet {
